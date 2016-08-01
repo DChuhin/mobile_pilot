43561cfb652ebb69a200509ca9b4501025f9061d
chukhindp <1223165>
2016-08-01 12:04:46 +0300

    add injector bean

--- src/main/java/com/pilot/configuration/MobilePilotConfig.java
+++ src/main/java/com/pilot/configuration/MobilePilotConfig.java
@@ -2,10 +2,11 @@ package com.pilot.configuration;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import javax.annotation.PostConstruct;
 
@@ -18,10 +19,21 @@ import javax.annotation.PostConstruct;
         "classpath:application.properties",
         "classpath:liquibase.properties",
         "classpath:jdbc.properties"
-})@ComponentScan({"com.pilot"})
+})
+@ComponentScan({"com.pilot"})
 public class MobilePilotConfig {
 
     private static final Logger LOGGER = Logger.getLogger(MobilePilotConfig.class);
+
+    /**
+     * PropertySourcesPlaceholderConfigurer
+     *
+     * @return PropertySourcesPlaceholderConfigurer
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     /**
      * Fixes Sonar error
