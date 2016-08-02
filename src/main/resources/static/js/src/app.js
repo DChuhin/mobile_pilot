d6e77c5a6f300a1c074b9491d73eb8dd3ff12382
chukhindp <1223165>
2016-08-02 13:08:42 +0300

    bootstrap

--- src/main/resources/static/js/src/app.js
+++ src/main/resources/static/js/src/app.js
@@ -2,18 +2,18 @@
     function authInterceptor(API, auth) {
         return {
             // automatically attach Authorization header
-            request: function(config) {
+            request: function (config) {
                 var token = auth.getToken();
-                if(config.url.indexOf(API) === 0 && token) {
-                    config.headers.Authorization = 'Bearer ' + token;
+                if (config.url.indexOf(API) === 0 && token) {
+                    config.headers['X-Pilot-Token'] = token;
                 }
 
                 return config;
             },
 
             // If a token was sent back, save it
-            response: function(res) {
-                if(res.config.url.indexOf(API) === 0 && res.data.token) {
+            response: function (res) {
+                if (res.config.url.indexOf(API) === 0 && res.data.token) {
                     auth.saveToken(res.data.token);
                 }
 
@@ -25,32 +25,20 @@
     function authService($window) {
         var self = this;
 
-        // Add JWT methods here
-        self.parseJwt = function(token) {
-            var base64Url = token.split('.')[1];
-            var base64 = base64Url.replace('-', '+').replace('_', '/');
-            return JSON.parse($window.atob(base64));
-        };
 
-        self.saveToken = function(token) {
+        self.saveToken = function (token) {
             $window.localStorage['jwtToken'] = token;
         };
 
-        self.getToken = function() {
+        self.getToken = function () {
             return $window.localStorage['jwtToken'];
         };
 
-        self.isAuthed = function() {
-            var token = self.getToken();
-            if(token) {
-                var params = self.parseJwt(token);
-                return Math.round(new Date().getTime() / 1000) <= params.exp;
-            } else {
-                return false;
-            }
+        self.isAuthed = function () {
+            return self.getToken();
         };
 
-        self.logout = function() {
+        self.logout = function () {
             $window.localStorage.removeItem('jwtToken');
         }
     }
@@ -62,10 +50,7 @@
         };
         // add authentication methods here
         self.login = function (username, password) {
-            return $http.post(API + '/auth/login', {
-                username: username,
-                password: password
-            })
+            return $http.post(API + '/token?username=' + username + "&password=" + password)
         };
 
     }
@@ -98,7 +83,7 @@
         .factory('authInterceptor', authInterceptor)
         .service('user', userService)
         .service('auth', authService)
-        .constant('API', 'http://test-routes.herokuapp.com')
+        .constant('API', 'http://localhost:8080')
         .config(function ($httpProvider) {
             $httpProvider.interceptors.push('authInterceptor');
         })
